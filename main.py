fa24f30d188158885808978510b4ae24b7326ebc
Anurag <Negianurag99@gmail.com>
2025-01-20 18:12:33 +0530

    made it to use a single file

--- main.py
+++ main.py
@@ -8,37 +8,56 @@ import tkinter as tk
 from tkinter import ttk
      
 
-root = tk.Tk()
-root.title("DOTA_HEROES")
-title = root.title()
+def main():
+    root = tk.Tk()
+    root.title("DOTA_HEROES")
+    title = root.title()
 
-window_width = 1980
-window_height = 1080
+    window_width = 600 
+    window_height = 400 
 
-#Screen dimensions
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+    #Screen dimensions
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    
+    #centre point 
+    centre_x = int(screen_width/2 - window_width/2)
+    centre_y = int(screen_height/2 - window_height/2)
+    
+    #set the position of the window
+    root.geometry(f"{window_width}x{window_height}+{centre_x}+{centre_y}")
+    
+    root.columnconfigure(0,weight=1)
+    
+    input_frame, keyword_entry = create_input_frame(root)
+    input_frame.grid(column=0, row=0)
+    
+    button_frame = create_button_frame(root)
+    button_frame.grid(column=1, row=0)
+    
+    root.mainloop()
 
-#centre point 
-centre_x = int(screen_width/2 - window_width/2)
-centre_y = int(screen_height/2 - window_height/2)
+def create_input_frame(container)->tuple[ttk.Frame, ttk.Entry]:
+    frame = ttk.Frame(container)
+    frame.columnconfigure(0, weight=1)
+    #Find Hero
+    ttk.Label(frame, text="Hero Name").grid(column=0, row=0, sticky=tk.W)
+    keyword = ttk.Entry(frame, width=30)
+    keyword.focus()
+    keyword.grid(column=1, row=0, sticky=tk.W)
+    return frame, keyword
 
-#set the position of the window
-root.geometry(f"{window_width}x{window_height}+{centre_x}+{centre_y}")
 
-tk.Label(root, text='Classic Label').pack()
-ttk.Label(root, text='Themed Label').pack()
- 
-frame = ttk.Frame(root)
-frame.pack(fill= "both", expand=True, padx=10, pady=10)
-search_bar = ttk.Entry(frame)
-search_bar.pack(ipadx= 20, ipady=5,expand=True, anchor="center", pady = 10)
+def create_button_frame(container)->ttk.Frame:
+    frame = ttk.Frame(container)
+    frame.columnconfigure(0, weight=1)
+    #search button
+    ttk.Button(frame, text="Search", command=lambda: search_the_string()).grid(column=0, row=0)
+    return frame
 
-# search_button = ttk.Button(root, text="Search", command=lambda: root.quit())
-# search_button.pack(ipadx= 5, 
-#                    ipady= 5,
-#                    expand=True)
-print(title)
-# message = tk.Label(root, text="DOTA, World")
-# message.pack()
-root.mainloop()
+
+def search_the_string(): 
+    print("word")
+
+if __name__ == "__main__":
+    main()
